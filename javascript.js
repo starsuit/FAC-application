50cbce37755c3c7c94b70dab23e1dc6ed124e17c
Sam <tygersam@hotmail.com>
2018-12-20 13:15:09 +0000

    generalise JS to other slides

--- javascript.js
+++ javascript.js
@@ -1,27 +1,44 @@
-var down = document.querySelector(".down-button");
-var up = document.querySelector(".up-button");
-var slider = document.querySelector(".img");
+var down = document.querySelectorAll(".down-button");
+var up = document.querySelectorAll(".up-button");
+var slider = document.querySelectorAll(".img");
 
-console.log(down, slider);
-function slideDown() {
-  slider.style.transform = `translatey(100%)`;
+console.log(down);
+
+function slideDown(i) {
+  console.log("down");
+  slider[i].style.transform = `translatey(100%)`;
   window.setTimeout(function() {
-    down.style.display = "none";
+    down[i].style.display = "none";
   }, 600);
   window.setTimeout(function() {
-    up.style.display = "block";
+    up[i].style.display = "block";
   }, 800);
 }
 
-function slideUp() {
-  slider.style.transform = `translatey(0%)`;
+function slideUp(i) {
+  console.log("up");
+  slider[i].style.transform = `translatey(0%)`;
   window.setTimeout(function() {
-    up.style.display = "none";
+    up[i].style.display = "none";
   }, 500);
   window.setTimeout(function() {
-    down.style.display = "block";
+    down[i].style.display = "block";
   }, 700);
 }
 
-down.addEventListener("click", slideDown);
-up.addEventListener("click", slideUp);
+for (let i = 0; i < down.length; i++) {
+  let choice = down[i];
+  choice.addEventListener("click", function() {
+    slideDown(i);
+  });
+}
+
+for (let i = 0; i < up.length; i++) {
+  let choice = up[i];
+  choice.addEventListener("click", function() {
+    slideUp(i);
+  });
+}
+
+// down.addEventListener("click", slideDown);
+// up.addEventListener("click", slideUp);
