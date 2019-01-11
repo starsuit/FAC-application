0439232b62ede127293a5a2fac979b85bfb0d7cb
Sam <tygersam@hotmail.com>
2019-01-11 15:20:37 +0000

    change textboxes so clicking in picture slides it down

--- javascript.js
+++ javascript.js
@@ -12,6 +12,18 @@ function slideDown(i) {
   }, 600);
 }
 
+function tester(i) {
+  if (window.getComputedStyle(up[i]).display === `none`) {
+    slider[i].style.transform = `translatey(100%)`;
+    window.setTimeout(function() {
+      down[i].style.display = "none";
+    }, 300);
+    window.setTimeout(function() {
+      up[i].style.display = "block";
+    }, 600);
+  }
+}
+
 function slideUp(i) {
   slider[i].style.transform = `translatey(0%)`;
   window.setTimeout(function() {
@@ -33,6 +45,27 @@ for (let i = 0; i < up.length; i++) {
   let choice = up[i];
   choice.addEventListener("click", function() {
     slideUp(i);
+  });
+}
+
+for (let i = 0; i < slider.length; i++) {
+  let choice = slider[i];
+  choice.addEventListener("click", function() {
+    tester(i);
+  });
+}
+
+for (let i = 0; i < slider.length; i++) {
+  let choice = slider[i];
+  choice.addEventListener("mouseover", function() {
+    down[i].style.background = `hsla(36, 100%, 33%, 0.5)`;
+  });
+}
+
+for (let i = 0; i < slider.length; i++) {
+  let choice = slider[i];
+  choice.addEventListener("mouseout", function() {
+    down[i].style.background = `transparent`;
   });
 }
 
